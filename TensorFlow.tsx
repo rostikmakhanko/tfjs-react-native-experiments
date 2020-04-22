98659140db65385a6831c4f2e0d078102df437a3
Rostyslav Makhanko <rostyslavm@wix.com>
2020-04-22 17:07:31 +0300

    Add training and test data

--- TensorFlow.tsx
+++ TensorFlow.tsx
@@ -2,10 +2,32 @@ import * as React from "react";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-react-native";
 import { Text, View } from "react-native";
+import * as iris from "./iris.json";
+import * as irisTesting from "./testing.json";
 
 type State = {
   isTfReady: boolean;
 };
+
+const trainingData = tf.tensor2d(
+  iris.map((item) => [
+    item.sepal_length,
+    item.sepal_width,
+    item.petal_length,
+    item.petal_width,
+  ]),
+  [130, 4]
+);
+
+const testingData = tf.tensor2d(
+  irisTesting.map((item) => [
+    item.sepal_length,
+    item.sepal_width,
+    item.petal_length,
+    item.petal_width,
+  ]),
+  [14, 4]
+);
 
 export class TensorFlow extends React.Component<any, State> {
   constructor(props: any) {
